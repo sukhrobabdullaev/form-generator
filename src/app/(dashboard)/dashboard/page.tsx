888cba3085da6cb5613d9906ac223efd65680720
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-02-23 08:54:23 +0500

    google probvider

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import React from "react";
 
 const Dashboard = async () => {
   const session = await getServerSession();
