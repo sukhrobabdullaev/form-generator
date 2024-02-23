94f54c727d67c2c67a2e57dcea992bd25c620ec7
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-02-23 15:56:49 +0500

    api route fixed for vercel

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
